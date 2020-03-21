093db89b1f5de1f11d7700b63c09aae250dab29b
Armin Babaei <armin.babaei@gmail.com>
2020-03-21 19:53:11 -0400

    Docker Config

--- project/__init__.py
+++ project/__init__.py
@@ -1,9 +1,8 @@
 # project/__init__.py
 
-
+import os
 from flask import Flask, jsonify
 from flask_restx import Resource, Api
-
 
 # instantiate the app
 app = Flask(__name__)
@@ -11,7 +10,8 @@ app = Flask(__name__)
 api = Api(app)
 
 # set config
-app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
 
 
 class Ping(Resource):
